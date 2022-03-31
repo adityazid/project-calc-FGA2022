54a9bdb2376486b7066f9325083e13d8ababb88a
Aditya Yazid Rizqi <101079812+adityazid@users.noreply.github.com>
2022-03-31 13:41:41 +0700

    membuat mini project kalkulator
    - membuat desain dari HTML & CSS
    - membuat kalkulator berfungsi di Javascript
    - menambahkan fitur font untuk kalkulator

---
+++ project-calculator/script.js
@@ -0,0 +1,111 @@
+const numbers = document.querySelectorAll(".number");
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (e) => {
+        updateScreen(e.target.value);
+    });
+});
+
+const calcScreen = document.querySelector('.calculator-screen');
+const updateScreen = (number) => {
+    calcScreen.value = number;
+}
+
+let prevNum = "";
+let calcOpr = "";
+let currNum = "";
+const inputNumber = (number) => {
+    if (currNum === "0") {
+        currNum = number
+    }
+    else {
+        currNum += number
+    }
+}
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (e) => {
+        inputNumber(e.target.value)
+        updateScreen(currNum);
+    });
+});
+
+const oper = document.querySelectorAll(".operator");
+
+oper.forEach((opr) => {
+    opr.addEventListener("click", (e) => {
+       inptOpt(e.target.value);
+    });
+});
+
+const inptOpt = (oper) => {
+    prevNum = currNum
+    calcOpr = oper
+    currNum = ""
+
+    if (calcOpr === "") {
+        prevNum = currNum
+    }
+    calcOpr = oper
+    currNum = "0"
+}
+
+const equalSign = document.querySelector(".equal-sign");
+equalSign.addEventListener("click", () => {
+    calculate()
+    updateScreen(currNum)
+})
+
+const calculate = () => {
+    let result = ''
+    switch (calcOpr) {
+        case "+":
+            result = parseFloat(prevNum) + parseFloat(currNum)
+            break;
+
+        case "-":
+            result = parseFloat(prevNum) - parseFloat(currNum)
+            break;
+
+        case "*":
+            result = parseFloat(prevNum) * parseFloat(currNum)
+            break;
+
+        case "/":
+            result = parseFloat(prevNum) / parseFloat(currNum)
+            break;
+    
+        default:
+            break;
+    }
+    currNum = result
+    calcOpr = ""
+}
+
+const clearAll = () => {
+    prevNum = ""
+    calcOpr = ""
+    currNum = "0"
+}
+
+const clearBtn = document.querySelector(".all-clear");
+
+clearBtn.addEventListener("click", () => {
+    clearAll()
+    updateScreen(currNum)
+
+})
+
+inputDecimal = (dot) => {
+    if (currNum.includes(".")) {
+        return
+    }
+    currNum += dot
+}
+
+const decimal = document.querySelector(".decimal");
+decimal.addEventListener("click", (e) => {
+   inputDecimal(e.target.value)
+   updateScreen(currNum)
+})
+
